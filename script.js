5a63f4b637c96c09e197c1022121e71f984787f3
Alex <alex.fahrer@hotmail.com>
2024-01-13 11:12:19 +0100

    Refactoring and adding infos to csv

--- script.js
+++ script.js
@@ -19,18 +19,36 @@ let cookies = document.cookie.split("; ");
 let worldCookie = cookies.find((row) => row.startsWith("worldName="));
 let datacenterCookie = cookies.find((row) => row.startsWith("datacenterName="));
 let regionCookie = cookies.find((row) => row.startsWith("region="));
+let checkboxCookie = cookies.find((row) =>
+  row.startsWith("onlySameDatacenter=")
+);
 
-if (worldCookie) {
+// if there are no cookies, set the worldName, datacenterName, and region cookies to default values: "Phoenix", "Light", "Europe", and "onlySameDatacenter" to false
+if (!worldCookie) {
+  document.cookie = "worldName=Phoenix; SameSite=Lax";
+} else {
   worldName = worldCookie.split("=")[1];
 }
 
-if (datacenterCookie) {
+if (!datacenterCookie) {
+  document.cookie = "datacenterName=Light; SameSite=Lax";
+} else {
   datacenterName = datacenterCookie.split("=")[1];
 }
 
-if (regionCookie) {
+if (!regionCookie) {
+  document.cookie = "region=Europe; SameSite=Lax";
+} else {
   region = regionCookie.split("=")[1];
 }
+
+if (!checkboxCookie) {
+  document.cookie = "onlySameDatacenter=false; SameSite=Lax";
+} else {
+  onlySameDatacenter = checkboxCookie.split("=")[1];
+}
+
+console.log(document.cookie);
 
 // DROPZONE FUNCTIONS
 
@@ -72,13 +90,13 @@ fetch("worlds.json") // replace 'worlds.json' with the path to your JSON file
     // Create a select element
     let select = document.createElement("select");
 
-    // Create a placeholder option
-    let placeholder = document.createElement("option");
-    placeholder.value = "";
-    placeholder.text = "Select World";
-    placeholder.disabled = true;
-    placeholder.selected = true;
-    select.appendChild(placeholder);
+    // // Create a placeholder option
+    // let placeholder = document.createElement("option");
+    // placeholder.value = "";
+    // placeholder.text = "Select World";
+    // placeholder.disabled = true;
+    // placeholder.selected = true;
+    // select.appendChild(placeholder);
 
     // Create an option for each world
     data.forEach((world) => {
@@ -155,7 +173,7 @@ fetch("worlds.json") // replace 'worlds.json' with the path to your JSON file
     // Create a new div for the checkbox and label
     let checkboxDiv = document.createElement("div");
 
-    // Create the checkbox
+    // Create a checkbox for "Only same datacenter?"
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.id = "onlySameDatacenter";
@@ -172,12 +190,27 @@ fetch("worlds.json") // replace 'worlds.json' with the path to your JSON file
     // Append the new div to the original div
     div.appendChild(checkboxDiv);
 
-    // Add event listener to the checkbox
-    checkbox.addEventListener("change", function () {
+    // Add an onchange event to the checkbox to store the selected option as a cookie
+    checkbox.onchange = function () {
+      document.cookie = "onlySameDatacenter=" + this.checked + "; SameSite=Lax";
+      // If a file has been selected, process it
       if (file) {
         processFile(file);
       }
-    });
+    };
+
+    // Check if the onlySameDatacenter cookie exists and set the checkbox
+    let checkboxCookie = cookies.find((row) =>
+      row.startsWith("onlySameDatacenter=")
+    );
+
+    if (checkboxCookie) {
+      let onlySameDatacenter = checkboxCookie.split("=")[1];
+      checkbox.checked = onlySameDatacenter === "true";
+    }
+
+    // Append the checkbox to the div
+    div.appendChild(checkboxDiv);
 
     // Append the select element to the div
     div.appendChild(select);
@@ -205,11 +238,31 @@ fetch("worlds.json") // replace 'worlds.json' with the path to your JSON file
       // Join csv array into a string with newline characters between each row
       let csvString = csv.join("\n");
 
-      // Get the name of the imported file
+      // Add the totals to a new row at the end of the CSV after a blank line
+      csvString += "\n\n";
+      csvString += `Total (Price on ${worldName}): ${totalWorldNamePrice.toLocaleString()} Gil\nTotal (Cheapest Price): ${totalCheapestPrice.toLocaleString()} Gil`;
+
+      let date = new Date();
+      let month = (date.getMonth() + 1).toString().padStart(2, "0"); // Add leading zeros to the month
+      let day = date.getDate().toString().padStart(2, "0"); // Add leading zeros to the day
+      let year = date.getFullYear().toString().slice(-2); // Get the last two digits of the year
+      let currentDate = `${month}.${day}.${year}`;
+
+      // get the current time
+      let hours = date.getHours().toString().padStart(2, "0"); // Add leading zeros to the hours
+      let minutes = date.getMinutes().toString().padStart(2, "0"); // Add leading zeros to the minutes
+      let currentTime = `${hours}:${minutes}`;
+
+      // add the date and current time to the csv
+      csvString += `\nDate / Time: ${currentDate} / ${currentTime}`;
+
       let importedFileName = file.name;
 
-      // Replace the extension with .csv
-      let exportedFileName = importedFileName.replace(/\.[^/.]+$/, ".csv");
+      importedFileName =
+        importedFileName.replace(/\.[^/.]+$/, "") + "_" + currentDate; // Remove the extension from the imported file name and append the date
+
+      // Add the .csv extension
+      let exportedFileName = importedFileName + ".csv";
 
       // Use the exportedFileName when saving the CSV file
       let blob = new Blob([csvString], { type: "text/csv;charset=utf-8;" });
@@ -248,6 +301,12 @@ fetch("items.json") // replace 'items.json' with the path to your JSON file
   .then((data) => (jsonData = data))
   .catch((error) => console.error("Error:", error));
 
+// Fetch the item names data when the page loads
+fetch("itemNames.json") // replace 'itemNames.json' with the path to your JSON file
+  .then((response) => response.json())
+  .then((data) => (itemNames = data))
+  .catch((error) => console.error("Error:", error));
+
 // PROCESS FILES
 
 function processFile(file) {
@@ -277,6 +336,7 @@ function processFile(file) {
       let headerRow = document.createElement("tr");
       [
         "Item",
+        "ItemDE",
         "Amount",
         "ID",
         "Is Craftable?",
@@ -304,8 +364,12 @@ function processFile(file) {
         let itemData = jsonData.find((item) => item.name === itemName.trim());
         if (itemData) {
           let row = document.createElement("tr");
+
+          let itemNameDE = itemNames[itemData.id].de;
+
           let cells = [
             itemName,
+            itemNameDE,
             itemAmount,
             itemData.id,
             itemData["is craftable?"],
@@ -332,7 +396,7 @@ function processFile(file) {
           }
 
           // Fetch data from Marketboard endpoint
-          let fetchPromise1 = fetch(
+          fetch(
             `https://universalis.app/api/v2/${worldName}/${itemData.id}?listings=${listingAmount}&entries=0&hq=0&fields=itemID%2Clistings.lastReviewTime%2Clistings.total`
           )
             .then((response) => response.json())
@@ -357,20 +421,20 @@ function processFile(file) {
                 }
 
                 // Update "Price on..." cell with median price, rounded to 1
-                row.cells[9].innerText = Math.round(median);
+                row.cells[10].innerText = Math.round(median);
               } else {
                 // Set cell text to 'N/A' if listings array is empty
-                row.cells[9].innerText = "";
+                row.cells[10].innerText = "";
               }
               // Calculate totals
-              let priceOnWorldName = row.cells[9].innerText;
+              let priceOnWorldName = row.cells[10].innerText;
               if (priceOnWorldName) {
                 totalWorldNamePrice += itemAmount * priceOnWorldName;
               }
             })
             .catch((error) => console.error("Error:", error));
 
-          fetchPromises.push(fetchPromise1);
+          // fetchPromises.push(fetchPromise1);
 
           // check if the "Only same datacenter?" checkbox is checked
           let checkbox = document.getElementById("onlySameDatacenter");
@@ -383,7 +447,7 @@ function processFile(file) {
           }
 
           // Fetch data from Marketboard endpoint
-          let fetchPromise2 = fetch(
+          fetch(
             `https://universalis.app/api/v2/${searchRange}/${itemData.id}?listings=${listingAmount}&entries=0&hq=0&fields=itemID%2Clistings.worldName%2Clistings.lastReviewTime%2Clistings.total`
           )
             .then((response) => response.json())
@@ -408,7 +472,7 @@ function processFile(file) {
                 );
 
                 // Fetch data from Marketboard endpoint
-                let fetchPromise3 = fetch(
+                fetch(
                   `https://universalis.app/api/v2/${searchRange}/${itemData.id}?listings=${listingAmount}&entries=0&hq=0&fields=itemID%2Clistings.worldName%2Clistings.lastReviewTime%2Clistings.total`
                 )
                   .then((response) => response.json())
@@ -435,58 +499,51 @@ function processFile(file) {
                       }
 
                       // Update "Cheapest World" cell with most common world
-                      row.cells[10].innerText = mostCommonWorld;
+                      row.cells[11].innerText = mostCommonWorld;
 
                       // Update "Cheapest Price" cell with median price, rounded to 1
-                      row.cells[11].innerText = Math.round(median);
+                      row.cells[12].innerText = Math.round(median);
 
                       // Calculate totals
                       totalCheapestPrice += itemAmount * Math.round(median);
                     } else {
                       // Set cell text to 'N/A' if listings array is empty
-                      row.cells[10].innerText = "";
                       row.cells[11].innerText = "";
+                      row.cells[12].innerText = "";
                     }
-
-                    resolve(); // Resolve the promise when the totalCheapestPrice update has completed
                   })
                   .catch((error) => {
                     console.error("Error:", error);
-                    reject(error); // Reject the promise if an error occurs
                   });
 
-                fetchPromises.push(fetchPromise3);
+                // fetchPromises.push(fetchPromise2);
               } else {
                 // Set cell text to 'N/A' if listings array is empty
-                row.cells[10].innerText = "";
                 row.cells[11].innerText = "";
+                row.cells[12].innerText = "";
               }
             })
-            .catch((error) => console.error("Error:", error));
+            .catch((error) => {
+              console.error("Error:", error);
+            });
 
-          fetchPromises.push(fetchPromise2);
+          // fetchPromises.push(fetchPromise3);
 
           table.appendChild(row);
         }
-        // Wait for all fetch requests to complete
-        Promise.all(fetchPromises)
-          .then(() => {
-            // Append the table to the output div
-            output.appendChild(table);
-
-            // Always show the text
-            let totalsContainer = document.getElementById("totals");
-            totalsContainer.innerHTML = `Total (Price on ${worldName}): <br>Total (Cheapest Price): `;
-
-            // Wait for 1 second before showing the total values
-            setTimeout(() => {
-              totalsContainer.innerHTML = `Total (Price on ${worldName}): ${totalWorldNamePrice.toLocaleString()} Gil<br>Total (Cheapest Price): ${totalCheapestPrice.toLocaleString()} Gil`;
-            }, 1000);
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-          });
       });
+
+      // Append the table to the output div
+      output.appendChild(table);
+
+      // Always show the text
+      let totalsContainer = document.getElementById("totals");
+      totalsContainer.innerHTML = `Total (Price on ${worldName}): <br>Total (Cheapest Price): `;
+
+      // Wait for 1 second before showing the total values
+      setTimeout(() => {
+        totalsContainer.innerHTML = `Total (Price on ${worldName}): ${totalWorldNamePrice.toLocaleString()} Gil<br>Total (Cheapest Price): ${totalCheapestPrice.toLocaleString()} Gil`;
+      }, 1000);
     };
 
     reader.readAsText(file);
